3244afd59d905be21c69bb08622936bcca32f20c
Tana0910 <32482176+Tana0910@users.noreply.github.com>
2023-01-09 19:01:10 +0900

    comment on hitchhike/main.cpp

--- hitchhike/main.cpp
+++ hitchhike/main.cpp
@@ -3,6 +3,8 @@
 
 int main()
 {
+    // s リテラルを付けて string 型のオブジェクトを構築する(仕様: N3642)
+    // https://cpprefjp.github.io/reference/string/basic_string/op_s.html
     std::cout << "Hello"s << std::endl;
     return 0;
 }
