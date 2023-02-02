486d14acfe4a822b0fa53e1251a692c282475afe
Tana0910 <32482176+Tana0910@users.noreply.github.com>
2023-02-02 23:40:15 +0900

    add howtodebut/printfdebug directory
     Changes to be committed:
    	new file:   howtodebug/printfdebug/Makefile
    	new file:   howtodebug/printfdebug/main.cpp

---
+++ howtodebug/printfdebug/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    std::vector<int> v = { 3, 8, 2, 5, 6, 9, 4, 1, 7 };
+    auto size = v.size();
+
+    // 洗濯ソートで vector の要素をソートする
+    for (std::size_t head = 0; head != size; ++head)
+    {
+        auto min = head;
+        for (std::size_t index = head + 1; index != size; ++index)
+        {
+            if (v.at(index) < v.at(min))
+            {
+                min = index;
+            }
+        }
+        auto temp = v.at(head);
+        v.at(head) = v.at(min);
+        v.at(min) = v.at(head); // わざと間違えている
+        // 正しくは下記の通り
+        // v.at(min) = temp;
+    }
+
+    // 出力
+    for (std::size_t i = 0; i != size; ++i)
+    {
+        std::cout << v.at(i) << " ";
+    }
+    std::cout << std::endl;
+    return 0;
+}
