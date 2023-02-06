56899d04f5b813936ee997172bc94fdd355c0dd6
Tana0910 <32482176+Tana0910@users.noreply.github.com>
2023-02-06 22:31:47 +0900

    update iterator/main.cpp
     Changes to be committed:
    	modified:   iterator/main.cpp

--- iterator/main.cpp
+++ iterator/main.cpp
@@ -13,5 +13,30 @@ int main()
     std::cout << *i << std::endl; // 2
     --i;
     std::cout << *i << std::endl; // 0
+
+    // イテレータの比較
+
+    auto itrx = std::begin(v);
+    auto itry = itrx;
+    std::cout << std::boolalpha << (itrx == itry) << std::endl;
+    std::cout << (itrx != itry) << std::endl;
+
+    ++itrx;
+
+    std::cout << (itrx == itry) << std::endl;
+
+    // 最後の次の要素
+
+    std::vector<int> v1 = { 10, 20, 30 };
+    auto itr_x = std::begin(v1);
+    auto itr_y = std::end(v1);
+    std::cout << (itr_x == itr_y) <<std::endl;
+    ++itr_x;
+    std::cout << (itr_x == itr_y) <<std::endl;
+    ++itr_x;
+    std::cout << (itr_x == itr_y) <<std::endl;
+    ++itr_x;
+    std::cout << (itr_x == itr_y) <<std::endl;
+
     return 0;
 }
