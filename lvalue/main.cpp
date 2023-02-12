17278a125d80445bd2b817a32c37d87ec85bfde0
Tana0910 <32482176+Tana0910@users.noreply.github.com>
2023-02-12 17:45:56 +0900

    update lvalue program
     Changes to be committed:
    	modified:   lvalue/main.cpp

--- lvalue/main.cpp
+++ lvalue/main.cpp
@@ -11,8 +11,53 @@ int main()
     int& ref = a;
     ref = 3;
 
-    std::cout << "a = "s << a << std::endl; // 3
-    std::cout << "ref = "s << ref << std::endl; // 3
+    std::cout << "a = "s << a << std::endl; // a = 3
+    std::cout << "ref = "s << ref << std::endl; // ref = 3
+
+    // lvalue リファレンスは必ず初期化しなければならない
+    // int& ref_err; // コンパイルエラー
+
+    auto assign_incorrect = [](int x)
+    {
+        x = 5;
+    };
+
+    a = 0;
+    assign_incorrect(a);
+    std::cout << "a = "s << a <<  std::endl; // a = 0;
+
+    auto assign_correct = [](int& x)
+    {
+        x = 5;
+    };
+
+    a = 0;
+    assign_correct(a);
+    std::cout << "a = "s << a << std::endl; // a = 5
+
+    auto swap_incorrect = [](auto a, auto b)
+    {
+        auto temp = a;
+        a = b;
+        b = temp;
+    };
+    int x = 10, y = 20;
+    swap_incorrect(x, y);
+    std::cout << "(x, y) = ("s << x << ", "s << y << ")" << std::endl;
+
+    auto swap_correct = [](auto& a, auto& b)
+    {
+        auto temp = a;
+        a = b;
+        b = temp;
+    };
+    int z = 123, w = 456;
+    swap_correct(z, w);
+    std::cout << "(z, w) = ("s << z << ", "s << w << ")" << std::endl;
+
+    z = 1234, w = 5678;
+    std::swap(z, w);
+    std::cout << "(z, w) = ("s << z << ", "s << w << ")" << std::endl;
 
     return 0;
 }
